377e993e1755c25faeac38a2829c68d469ad8fa7
BlakeMeloche <bjmeloche@mavs.coloradomesa.edu>
2025-10-23 14:22:11 -0600

    Add files via upload

---
+++ 11.22.10.py
@@ -0,0 +1,9 @@
+yap = "I have old shoes and an old house and an old car, I sold all of them for a bold price of forty dollars."
+word = "old"
+word2 = "new"
+
+yap2 = yap.split(word)
+
+yap3 = word2.join(yap2)
+print("Before action: " + yap + "\n")
+print("After join: " + yap3)
